b29fe6a91b8057cc4e91007197ada87eb3012a71
Ruban <65346010+rubansk@users.noreply.github.com>
2020-06-09 23:34:21 +0800

    Adding Memorize + Notes

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by SK Ruban on 9/6/20.
+//  Copyright © 2020 SK Ruban. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { _ in
+                CardView(isFaceUp: true)
+            }
+        }
+        .foregroundColor(Color.orange)
+        .padding()
+        .font(Font.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
