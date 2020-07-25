9894ce7ed317f5828ecbb6698778b0f62a6127c7
Ruban <65346010+rubansk@users.noreply.github.com>
2020-07-25 14:33:59 +0800

    Adding Lecture 12

---
+++ 12 - Enroute/Airport.swift
@@ -0,0 +1,102 @@
+//
+//  Airport.swift
+//  Enroute
+//
+//  Created by SK Ruban on 25/7/20.
+//  Copyright © 2020 SK Ruban. All rights reserved.
+//
+
+import CoreData
+import Combine
+
+extension Airport: Identifiable, Comparable {
+    static func withICAO(_ icao: String, context: NSManagedObjectContext) -> Airport {
+        
+        // look up ICAO in CoreData
+        let request = fetchRequest(NSPredicate(format: "icao_ = %@", icao))
+        // if it can't find it will return a empty array NOT nil
+        let airports = (try? context.fetch(request)) ?? []
+        if let airport = airports.first {
+            // if found return
+            return airport
+        } else {
+            // else create one and fetch from FlightAware
+            let airport = Airport(context: context)
+            airport.icao = icao
+            AirportInfoRequest.fetch(icao) { airportInfo in
+                self.update(from: airportInfo, context: context)
+            }
+            return airport
+        }
+        
+    }
+    
+    static func update(from info: AirportInfo, context: NSManagedObjectContext) {
+        if let icao = info.icao {
+            let airport = self.withICAO(icao, context: context)
+            airport.longitude = info.longitude
+            airport.latitude = info.latitude
+            airport.name = info.name
+            airport.location = info.location
+            airport.timezone = info.timezone
+            airport.objectWillChange.send()
+            airport.flightsTo.forEach { $0.objectWillChange.send() }
+            airport.flightsFrom.forEach { $0.objectWillChange.send() }
+            try? context.save()
+        }
+    }
+    
+    var flightsTo: Set<Flight> {
+        get { (flightsTo_ as? Set<Flight>) ?? [] }
+        set { flightsTo_ = newValue as NSSet }
+    }
+    
+    var flightsFrom: Set<Flight> {
+        get { (flightsFrom_ as? Set<Flight>) ?? []}
+        set { flightsFrom_ = newValue as NSSet}
+    }
+    
+    var icao: String {
+        get { icao_! } // TODO: maybe protect against when app ships?
+        set { icao_ = newValue }
+    }
+    
+    var friendlyName: String {
+        let friendly = AirportInfo.friendlyName(name: self.name ?? "", location: self.location ?? "")
+        return friendly.isEmpty ? icao : friendly
+    }
+    
+    public var id: String { icao }
+
+    public static func < (lhs: Airport, rhs: Airport) -> Bool {
+        lhs.location ?? lhs.friendlyName < rhs.location ?? rhs.friendlyName
+    }
+    
+    static func fetchRequest(_ predicate: NSPredicate) -> NSFetchRequest<Airport> {
+        let request = NSFetchRequest<Airport>(entityName: "Airport")
+        request.sortDescriptors = [NSSortDescriptor(key: "location", ascending: true)]
+        request.predicate = predicate
+        return request
+    }
+    
+    func fetchIncomingFlights() {
+        Self.flightAwareRequest?.stopFetching()
+        if let context = managedObjectContext {
+            Self.flightAwareRequest = EnrouteRequest.create(airport: icao, howMany: 90)
+            Self.flightAwareRequest?.fetch(andRepeatEvery: 60)
+            Self.flightAwareResultsCancellable = Self.flightAwareRequest?.results.sink { results in
+                for faflight in results {
+                    Flight.update(from: faflight, in: context)
+                }
+                do {
+                    try context.save()
+                } catch(let error) {
+                    print("couldn't save flight update to CoreData: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    private static var flightAwareRequest: EnrouteRequest!
+    private static var flightAwareResultsCancellable: AnyCancellable?
+}
---
+++ 12 - Enroute/Enroute/FilterFlights.swift
@@ -0,0 +1,79 @@
+//
+//  FilterFlights.swift
+//  Enroute
+//
+//  Created by CS193p Instructor on 5/12/20.
+//  Copyright © 2020 Stanford University. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilterFlights: View {
+    @FetchRequest(fetchRequest: Airport.fetchRequest(.all)) var airports: FetchedResults<Airport>
+    @FetchRequest(fetchRequest: Airline.fetchRequest(.all)) var airlines: FetchedResults<Airline>
+    
+    @Binding var flightSearch: FlightSearch
+    @Binding var isPresented: Bool
+    
+    @State private var draft: FlightSearch
+    
+    init(flightSearch: Binding<FlightSearch>, isPresented: Binding<Bool>) {
+        _flightSearch = flightSearch
+        _isPresented = isPresented
+        _draft = State(wrappedValue: flightSearch.wrappedValue)
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Destination", selection: $draft.destination) {
+                    ForEach(airports.sorted(), id: \.self) { airport in
+                        // Tag allows to put 'airport' into Binding $draft.destination
+                        // ! Binding has to be same type as Tag
+                        Text("\(airport.friendlyName)").tag(airport)
+                    }
+                }
+                Picker("Origin", selection: $draft.origin) {
+                    Text("Any").tag(Airport?.none)
+                    ForEach(airports.sorted(), id: \.self) { (airport: Airport?) in
+                        Text("\(airport?.friendlyName ?? "Any")").tag(airport)
+                    }
+                }
+                Picker("Airline", selection: $draft.airline) {
+                    Text("Any").tag(Airline?.none)
+                    ForEach(airlines.sorted(), id: \.self) { (airline: Airline?) in
+                        Text("\(airline?.friendlyName ?? "Any")").tag(airline)
+                    }
+                }
+                Toggle(isOn: $draft.inTheAir) { Text("Enroute Only") }
+            }
+            .navigationBarTitle("Filter Flights")
+            .navigationBarItems(leading: cancel, trailing: done)
+        }
+    }
+    
+    // Don't Make the Changes
+    var cancel: some View {
+        Button("Cancel") {
+            self.isPresented = false
+        }
+    }
+    
+    // Make the changes
+    var done: some View {
+        Button("Done") {
+            // Draft struct gets copied over
+            if self.draft.destination != self.flightSearch.destination {
+                self.draft.destination.fetchIncomingFlights()
+            }
+            self.flightSearch = self.draft
+            self.isPresented = false
+        }
+    }
+}
+
+//struct FilterFlights_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FilterFlights()
+//    }
+//}
