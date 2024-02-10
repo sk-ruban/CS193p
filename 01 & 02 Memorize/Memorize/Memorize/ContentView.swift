6068fe7518dda9eb9a5428fe46e96d49d5d8cdc4
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-02-10 16:30:55 +0800

    Add 2023 Lecture 02

--- 01 & 02 Memorize/Memorize/Memorize/ContentView.swift
+++ 01 & 02 Memorize/Memorize/Memorize/ContentView.swift
@@ -8,33 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻", "🎃", "🦇","🧛","⚰️","🪄","🔮","🧿","🦄","🍭","🧙","🧌"]
+    @State var cardCount = 4
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
         }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
         .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardCountAdjustor(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+            Spacer()
+            cardCountAdjustor(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+        }
+        .imageScale(.large)
         .font(.largeTitle)
+    }
+    
+    func cardCountAdjustor(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    let content: String
+    @State var isFaceUp = true
+    let base = RoundedRectangle(cornerRadius: 12)
     
     var body: some View {
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("👻")
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1:0)
+            base.fill().opacity(isFaceUp ? 0:1)
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# CS193p
-This repo details my progress for Stanford's CS193p (Developing Apps for iOS) - Spring 2023. I did this course previously for Spring 2020. You can view my progress for Spring 2020 [here](https://github.com/sk-ruban/CS193p/tree/master/2020).
+# 🎒 CS193p Spring 2023
+This repo details my progress for Stanford's CS193p (Developing Apps for iOS) - Spring 2023.
 
 My code, personal notes & assignment solutions (+ extra credit) are all listed in the table below. 
 Lectures, videos & corresponding project files from Stanford can be found [here](https://cs193p.sites.stanford.edu/2023).
 
+**I also did this course previously in Spring 2020.** You can view my files for that version [here](https://github.com/sk-ruban/CS193p/tree/master/2020).
+
 ### 🚧 Progress
 | Lectures | My Notes | My Code | Assignments |
 | --------------- | :-------------: | :-------------: | :-------------: |
-| [1: Basics of SwiftUI](https://www.youtube.com/watch?v=n1qabtjZ_jg) | - | [Memorize 01 & 02](https://github.com/sk-ruban/CS193p/tree/master/01%20%26%2002%20-%20Memorize) | - |
+| [1: Getting Started](https://www.youtube.com/watch?v=n1qabtjZ_jg) | - | [Memorize 01 & 02](https://github.com/sk-ruban/CS193p/tree/master/01%20%26%2002%20Memorize) | - |
+| [2: More SwiftUI](https://www.youtube.com/watch?v=sXiD-2XrkKQ) | - | [Memorize 01 & 02](https://github.com/sk-ruban/CS193p/tree/master/01%20%26%2002%20Memorize) | - |
 
 ### 📸 Screenshots
 
